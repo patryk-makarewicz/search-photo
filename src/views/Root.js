15c57162697824e6835fa937cc78a5d628c344fc
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-13 20:41:16 +0100

    feat: search input works without button, start search after click enter

--- src/views/Root.js
+++ src/views/Root.js
@@ -5,15 +5,17 @@ import GlobalStyle from 'theme/GlobalStyle';
 const Root = () => {
   const [photo, setPhoto] = useState('');
   const [result, setResult] = useState([]);
-  const { clientID } = '';
+  const clientID = '';
 
   const handleChange = (e) => {
     setPhoto(e.target.value);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     const url = `https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=${clientID}`;
-    axios.get(url).then((response) => setResult(response.data.results));
+    if (e.keyCode === 13) {
+      axios.get(url).then((response) => setResult(response.data.results));
+    }
   };
 
   return (
@@ -21,10 +23,13 @@ const Root = () => {
       <GlobalStyle />
       <div>
         <h1>Search photo</h1>
-        <input type="text" name="photo" placeholder="Search photo" onChange={handleChange} />
-        <button type="submit" onClick={handleSubmit}>
-          Search
-        </button>
+        <input
+          type="text"
+          name="photo"
+          placeholder="Search photo"
+          onChange={handleChange}
+          onKeyDown={(e) => handleSubmit(e)}
+        />
         {result.map((picture) => (
           <img key={picture.id} src={picture.urls.small} alt="Your search result" />
         ))}
