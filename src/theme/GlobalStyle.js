52b6ad85634e154849468ec590cc90df1aef6a97
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-13 13:26:52 +0100

    initial commit: template ready to work

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700');
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: "Montserrat", sans-serif;
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: white;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/views/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from 'theme/GlobalStyle';
+
+const Root = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <div>Hello</div>
+    </>
+  );
+};
+
+export default Root;
